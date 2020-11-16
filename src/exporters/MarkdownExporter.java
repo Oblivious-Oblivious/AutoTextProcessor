bae2b1e10c5f8f9d6e2f9a531ae92f512fafe3fd
Oblivious-Oblivious <me262thanos@gmail.com>
2020-11-16 06:05:48 +0200

    Crafted MarkdownExporter and tests.

--- src/exporters/MarkdownExporter.java
+++ src/exporters/MarkdownExporter.java
@@ -1,11 +1,91 @@
 package exporters;
 
 import datamodel.buildingblocks.Document;
+import datamodel.buildingblocks.FormatEnum;
+import datamodel.buildingblocks.LineBlock;
+import datamodel.buildingblocks.StyleEnum;
+import datamodel.files.FileHandler;
+import datamodel.files.WriteHandler;
 
 public class MarkdownExporter {
-    public MarkdownExporter(Document document, String outputFileName) {}
+    private Document document;
+    private String outputFileName;
+    private FileHandler handler;
+
+    private FileHandler createOutputFileHandler() {
+        return new WriteHandler(this.outputFileName);
+    }
+
+    private String getStyleNotation(LineBlock l) {
+        if(l.getStyle() == StyleEnum.H1)
+            return "#";
+        if(l.getStyle() == StyleEnum.H2)
+            return "##";
+        if(l.getStyle() == StyleEnum.OMITTED)
+            return null;
+        
+        /* NORMAL */
+        return "";
+    }
+
+    private String getFormatNotation(LineBlock l) {
+        if(l.getFormat() == FormatEnum.BOLD)
+            return "**";
+        if(l.getFormat() == FormatEnum.ITALICS)
+            return "_";
+        
+        /* REGULAR */
+        return "";
+    }
+
+    private void addParagraph(LineBlock l) {
+        StringBuilder parBlock = new StringBuilder();
+
+        for(String block : l.getLines()) {
+            // System.out.println("APP: `" + block + "`");
+            parBlock.append(block + " ");
+            // parBlock.append(" ");
+        }
+
+        String styleNotation = getStyleNotation(l);
+        String formatNotation = getFormatNotation(l);
+
+        /* TODO -> UGLY CODE */
+        if(styleNotation != null) { /* OMMITED */
+            if(styleNotation.equals("")) /* NORMAL */ {
+                handler.appendLine(formatNotation + parBlock + formatNotation);
+                System.out.println("A: `" + formatNotation + parBlock + formatNotation + "`");
+            }
+            else {
+                handler.appendLine(styleNotation + parBlock);
+                System.out.println("A: `" + styleNotation + parBlock + "`");
+            }
+            handler.appendLine("");
+        }
+    }
+
+    private int exportParagraphs(FileHandler handler) {
+        int par = 0;
+
+        for(LineBlock l : document.getLineblocks()) {
+            addParagraph(l);
+            par++;
+        }
+
+        /* TODO -> IF BELOW IS REMOVED FILE HANDLING CRASHES */
+        /* TODO -> JAVA GARBAGE COLLECTOR DOESNT WORK WTF ?!? */
+        this.handler.closeFD();
+
+        return par;
+    }
+
+    public MarkdownExporter(Document document, String outputFileName) {
+        this.document = document;
+        this.outputFileName = outputFileName;
+        this.handler = createOutputFileHandler();
+    }
 
     public int export() {
-        return 1;
+        return exportParagraphs(handler);
     }
 }
---
+++ src/test/exporters/TestMarkdownExporter.java
@@ -0,0 +1,41 @@
+package test.exporters;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import dataload.RawFileLineLoader;
+import datamodel.buildingblocks.Document;
+import datamodel.buildingblocks.Document.DocumentRawType;
+import exporters.MarkdownExporter;
+
+/**
+ * TestMarkdownExporter
+ */
+public class TestMarkdownExporter {
+    private Document getDocument(String filename) {
+        Document doc = new Document(filename, DocumentRawType.RAW);
+
+        /* Load the file */
+        RawFileLineLoader loader = new RawFileLineLoader();
+        loader.load(filename, doc.getLineblocks());
+
+        return doc;
+    }
+
+    @Test
+    public final void export_hippo_txt() {
+        Document doc = getDocument("C:\\Users\\roott\\Documents\\C++\\uoi\\java\\Panos\\2020\\project\\Resources\\SampleDocs\\hippocratesOath.txt");
+        MarkdownExporter ex = new MarkdownExporter(doc, "C:\\Users\\roott\\Documents\\C++\\uoi\\java\\Panos\\2020\\project\\Resources\\Outputs\\hippoTxt.md");
+
+        assertEquals(17, ex.export());
+    }
+
+    @Test
+    public final void export_hippo_html() {
+        Document doc = getDocument("C:\\Users\\roott\\Documents\\C++\\uoi\\java\\Panos\\2020\\project\\Resources\\SampleDocs\\hippocratesOath.html");
+        MarkdownExporter ex = new MarkdownExporter(doc, "C:\\Users\\roott\\Documents\\C++\\uoi\\java\\Panos\\2020\\project\\Resources\\Outputs\\hippo.md");
+
+        assertEquals(17, ex.export());
+    }
+}
