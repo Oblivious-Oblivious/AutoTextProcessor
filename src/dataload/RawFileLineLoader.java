8cdb3d9bdf84cdb3f0fb91d8453081de81091150
Oblivious-Oblivious <me262thanos@gmail.com>
2020-12-23 00:09:40 +0200

    Update RawFileLineLoader.java

--- src/dataload/RawFileLineLoader.java
+++ src/dataload/RawFileLineLoader.java
@@ -3,47 +3,78 @@ package dataload;
 import java.util.ArrayList;
 import java.util.List;
 
-import datamodel.buildingblocks.LineBlock;
-import datamodel.files.FileHandler;
-import datamodel.files.ReadHandler;
+import datamodel.LineBlock;
+import files.ReadHandler;
 
+/**
+ * @class RawFileLineLoader
+ * @brief main Loader controller class for reading values from the input files
+ */
 public class RawFileLineLoader implements ILoader {
-    private List<String> read_file(String filepath) {
-        List<String> sb = new ArrayList<>();
-        FileHandler handler = new ReadHandler(filepath);
+    /**
+     * handler -> A read file handler for simplified file I/O
+     * arraylist -> A List of Strings that will compose the
+     *              paragraph lines before inserted into the LineBlock List
+     */
+    private final ReadHandler handler;
+    private ArrayList<String> arraylist;
 
-        String data = handler.read_line();
-        while(data != null) {
-            sb.add(data);
-            data = handler.read_line();
+    /**
+     * @message process_line
+     * @brief Processes a single line and decides on creating the line_blocks
+     * @param line -> The line to process
+     * @return -> A boolean that will fail when we have end of file
+     */
+    private boolean process_line(String line, List<LineBlock> line_blocks) {
+        /* TODO -> FIX */
+
+        /* EOF */
+        if(line == null) {
+            if(this.arraylist.size() != 0)
+                line_blocks.add(new LineBlock(this.arraylist));
+            return false;
         }
 
-        return sb;
+        /* New Paragraph */
+        else if(line.equals("")) {
+            if(this.arraylist.size() > 0) {
+                line_blocks.add(new LineBlock(this.arraylist));
+                this.arraylist = new ArrayList<>();
+            }
+            return true;
+        }
+
+        /* Regular line in the middle of a paragraph */
+        else {
+            arraylist.add(line);
+            return true;
+        }
     }
 
-    public RawFileLineLoader() {}
+    /**
+     * @message fill_line_blocks
+     * @brief Processes all lines and fills the line_block list object with paragraphs
+     */
+    private void fill_line_blocks(List<LineBlock> line_blocks) {
+        while(process_line(this.handler.read_line(), line_blocks));
+    }
 
+    public RawFileLineLoader() {
+        this.handler = new ReadHandler();
+        this.arraylist = new ArrayList<>();
+    }
+
+    /**
+     * @message load
+     * @brief the main API call that handles files and loads to the line_blocks List
+     * @param filepath -> The file path to read from
+     * @param line_blocks -> The list to fill
+     */
     @Override
     public void load(String filepath, List<LineBlock> line_blocks) {
-        List<String> block = new ArrayList<>();
-        List<String> sb = read_file(filepath);
-
-        for(int i = 0; i < sb.size(); i++) {
-            if(sb.get(i).equals("")) {
-                if(sb.get(i-1).equals("")) {
-                    block = new ArrayList<>();
-                    continue;
-                }
-
-                line_blocks.add(new LineBlock(block));
-                block = new ArrayList<>();
-            }
-            else
-                block.add(sb.get(i));
-        }
-
-        if(block.size() > 0) {
-            line_blocks.add(new LineBlock(block));
+        if(this.handler.open(filepath)) {
+            fill_line_blocks(line_blocks);
+            this.handler.close();
         }
     }
 }
